c225fab2f922c80a492e9501461766955c4bfe79
byt3-m3 <cbaxtertech@gmail.com>
2021-12-15 14:10:01 -0500

    Added env for RabbitMQ

--- boe/env.py
+++ boe/env.py
@@ -6,6 +6,15 @@ _BANK_TRANSACTION_TABLE_ID = os.getenv("BANK_ACCOUNT_TABLE_ID", "bank_transactio
 _USER_ACCOUNT_TABLE_ID = os.getenv("USER_ACCOUNT_TABLE_ID", "user_account_table")
 _FAMILY_TABLE_ID = os.getenv("USER_ACCOUNT_TABLE_ID", "family_table")
 
+AMQP_USER = os.getenv("AMQP_USER")
+AMQP_PW = os.getenv("AMQP_PW")
+AMQP_HOST = os.getenv("AMQP_HOST")
+AMQP_PORT = os.getenv("AMQP_PORT")
+
+AMPQ_URL = os.getenv("AMPQ_URL", f"amqp://{AMQP_USER}:{AMQP_PW}@{AMQP_HOST}:{AMQP_PORT}")
+
+BANK_MANAGER_APP_QUEUE = os.getenv("BANK_MANAGER_APP_QUEUE", f"{STAGE}_bank_manager_app_queue".upper())
+
 MONGO_HOST = os.getenv("MONGO_HOST", "192.168.1.5")
 MONGO_PORT = os.getenv("MONGO_PORT", 27017)
 _APP_DB = os.getenv("APP_DB", "BOE_MVP")
