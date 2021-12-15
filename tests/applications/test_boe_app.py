ff081eae3bb89fcdd799f313d223d086f2da3e46
byt3-m3 <cbaxtertech@gmail.com>
2021-12-15 14:09:30 -0500

    Added tests for bank_manager app

--- tests/applications/test_boe_app.py
+++ tests/applications/test_boe_app.py
@@ -1,6 +1,19 @@
-from boe.applications import BankManagerApp
+from uuid import uuid4
 
+from boe.applications import (
+    BankManagerApp,
+    EstablishNewAccountEvent
+)
+from boe.lib.domains.bank_domain import BankDomainAggregate
 from pytest import fixture
+
+
+@fixture
+def establish_new_account_event(user_account_uuid):
+    return EstablishNewAccountEvent(
+        owner_id=uuid4(),
+        is_overdraft_protected=True
+    )
 
 
 @fixture
@@ -8,7 +21,19 @@ def bank_manager_app_testable():
     return BankManagerApp()
 
 
-def test_basic_test(bank_manager_app_testable):
+def _test_basic_test(bank_manager_app_testable):
     app = bank_manager_app_testable
 
     app.test_event()
+
+
+def test_bank_manager_app_when_handle_establish_new_account_event(
+        bank_manager_app_testable,
+        establish_new_account_event
+):
+    app = bank_manager_app_testable
+    event = establish_new_account_event
+    _id = app.handle_establish_new_account_event(event=event)
+
+    aggregate = app.repository.get(_id)
+    assert isinstance(aggregate, BankDomainAggregate)
