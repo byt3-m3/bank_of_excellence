95ef63a64e0384005b9e22d37998c8663b7fd32c
byt3-m3 <cbaxtertech@gmail.com>
2021-12-16 06:36:19 -0500

    Added new transcodings for UserManagerApp

--- boe/applications/transcodings.py
+++ boe/applications/transcodings.py
@@ -6,7 +6,57 @@ from boe.lib.domains.bank_domain import (
     BankTransactionMethodEnum,
     BankAccountStateEnum
 )
+from boe.lib.domains.user_domain import (
+    FamilyEntity,
+    UserAccountEntity,
+    SubscriptionTypeEnum,
+    UserAccountTypeEnum
+)
 from eventsourcing.persistence import Transcoding
+
+
+class UserAccountTypeEnumTranscoding(Transcoding):
+    type = UserAccountTypeEnum
+    name = "UserAccountTypeEnum"
+
+    def encode(self, o: UserAccountTypeEnum) -> int:
+        return o.value
+
+    def decode(self, d: int):
+        return UserAccountTypeEnum(d)
+
+
+class SubscriptionTypeEnumTranscoding(Transcoding):
+    type = SubscriptionTypeEnum
+    name = "SubscriptionTypeEnum"
+
+    def encode(self, o: SubscriptionTypeEnum) -> int:
+        return o.value
+
+    def decode(self, d: int):
+        return SubscriptionTypeEnum(d)
+
+
+class UserAccountEntityTranscoding(Transcoding):
+    type = UserAccountEntity
+    name = "UserAccountEntity"
+
+    def encode(self, o: UserAccountEntity) -> str:
+        return asdict(o)
+
+    def decode(self, d: dict):
+        return UserAccountEntity(**d)
+
+
+class FamilyEntityTranscoding(Transcoding):
+    type = FamilyEntity
+    name = "FamilyEntity"
+
+    def encode(self, o: FamilyEntity) -> str:
+        return asdict(o)
+
+    def decode(self, d: dict):
+        return FamilyEntity(**d)
 
 
 class BankAccountEntityTranscoding(Transcoding):
