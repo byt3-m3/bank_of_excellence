25269cf227ad5e449d819737257faec7d31d2478
byt3-m3 <cbaxtertech@gmail.com>
2021-12-19 00:47:14 -0500

    Added mocks and better test assertions

--- tests/lib/domains/test_persistence_domain.py
+++ tests/lib/domains/test_persistence_domain.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from uuid import UUID
 
 from boe.lib.domains.persistence_domain import PersistenceDomainFactory
@@ -5,21 +6,31 @@ from pytest import fixture
 
 
 @fixture
+def bank_domain_write_model_mock():
+    with patch("boe.lib.domains.persistence_domain.BankDomainWriteModel") as write_model_mock:
+        yield write_model_mock
+
+
+@fixture
 def persistence_aggregate_testable(set_persistence_test_env):
     return PersistenceDomainFactory.build_persistence_aggregate(
-        aggregate_id=UUID('eaaa7901-e49a-4bbd-ab64-410e061d7339')
+        aggregate_id=UUID('eaaa7901-e49a-4bbd-ab64-410e061d7339'),
+        aggregate_type='BankDomainAggregate'
+
     )
 
 
-def _test_persistence_aggregate_when_adding_new_request(persistence_aggregate_testable):
+def test_persistence_aggregate_when_created(persistence_aggregate_testable):
     aggregate = persistence_aggregate_testable
-
-    aggregate.new_persistence_request(payload={"data": "data"}, payload_type='TestType')
-
-    print(aggregate.pending_events)
+    assert len(aggregate.pending_events) == 1
+    assert aggregate.record.aggregate_id == UUID('eaaa7901-e49a-4bbd-ab64-410e061d7339')
 
 
-def test_persistence_aggregate_when_persisting_bank_aggregate(persistence_aggregate_testable):
+def test_persistence_aggregate_when_persisting_bank_aggregate(
+        bank_domain_write_model_mock,
+        persistence_aggregate_testable
+
+):
     aggregate = persistence_aggregate_testable
 
     payload = {
@@ -35,3 +46,6 @@ def test_persistence_aggregate_when_persisting_bank_aggregate(persistence_aggreg
     }
 
     aggregate.persist_bank_aggregate(payload=payload)
+
+    bank_domain_write_model_mock.assert_called()
+    assert len(aggregate.pending_events) == 2
