8e2d02dbc0eeba6007c27d1b6d4eba207e02d475
byt3-m3 <cbaxtertech@gmail.com>
2021-12-19 00:46:47 -0500

    development tests

--- pike_dev.py
+++ pike_dev.py
@@ -69,5 +69,7 @@ def publish_save_aggregate_event():
 
 
 if __name__ == "__main__":
-    account_id = publish_establish_new_account_event()
-    publish_new_transaction_event(account_id=account_id)
+    # account_id = publish_establish_new_account_event()
+    # publish_new_transaction_event(account_id=account_id)
+
+    publish_save_aggregate_event()
