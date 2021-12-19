db3bd095e76fd22018724e3541587401716d7f3c
byt3-m3 <cbaxtertech@gmail.com>
2021-12-19 13:49:27 -0500

    Added support for DLQ to be created.

--- boe/workers/bank_manager_worker.py
+++ boe/workers/bank_manager_worker.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import pika.exceptions
 from boe.applications.bank_domain_apps import BankDomainAppEventFactory
 from boe.applications.bank_domain_apps import (
     BankManagerApp
@@ -8,7 +9,8 @@ from boe.applications.bank_domain_apps import (
 )
 from boe.env import AMPQ_URL, BANK_MANAGER_WORKER_QUEUE, BANK_MANAGER_WORKER_SQLITE_EVENT_STORE
 from cbaxter1988_utils.log_utils import get_logger
-from cbaxter1988_utils.pika_utils import make_basic_pika_consumer
+from cbaxter1988_utils.pika_utils import make_basic_pika_consumer, PikaQueueServiceWrapper
+from eventsourcing.application import AggregateNotFound
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
 
@@ -45,21 +47,44 @@ def on_message_callback(ch: BlockingChannel, method: Basic.Deliver, properties: 
             result = handler(event=_event)
 
             logger.info(f'Processed ApplicationEvent={_event} - Handler={handler}')
-        except Exception as error:
 
-            ch.basic_nack(delivery_tag=method.delivery_tag)
+
+        except AggregateNotFound as err:
+            logger.warning(f"Handled AggregateNotFound Error: {err}")
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+            pass
+
+        except Exception as error:
+            logger.error(f'Unknown Error Encountered: {error}')
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
             raise
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 def main():
+    queue_service_wrapper = PikaQueueServiceWrapper(
+        amqp_url=AMPQ_URL
+    )
+
+    queue_service_wrapper.create_queue(
+        queue=BANK_MANAGER_WORKER_QUEUE,
+        dlq_support=True,
+        dlq_queue='BOE_ERRORS',
+        dlq_exchange='BOE_ERROR_EXCHANGE',
+        dlq_routing_key='BOE_ERROR_ROUTING_KEY'
+    )
+
     consumer = make_basic_pika_consumer(
         amqp_url=AMPQ_URL,
         queue=BANK_MANAGER_WORKER_QUEUE,
         on_message_callback=on_message_callback,
     )
-    consumer.run()
+    try:
+
+        consumer.run()
+    except pika.exceptions.ChannelClosedByBroker:
+        consumer.run()
 
 
 if __name__ == "__main__":
--- boe/workers/persistence_service_worker.py
+++ boe/workers/persistence_service_worker.py
@@ -2,12 +2,13 @@ import json
 import os
 from dataclasses import dataclass
 
+import pika.exceptions
 from boe.applications.persistence_domain_app import PersistenceServiceApp, PersistenceDomainAppEventFactory
 from boe.env import AMPQ_URL, PERSISTENCE_WORKER_QUEUE, PERSISTENCE_WORKER_SQLITE_EVENT_STORE
 from boe.lib.common_models import AppEvent
 from boe.lib.domains.bank_domain import BankDomainWriteModel
 from cbaxter1988_utils.log_utils import get_logger
-from cbaxter1988_utils.pika_utils import make_basic_pika_consumer
+from cbaxter1988_utils.pika_utils import make_basic_pika_consumer, PikaQueueServiceWrapper
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
 
@@ -43,29 +44,50 @@ def on_message_callback(ch: BlockingChannel, method: Basic.Deliver, properties: 
     logger.info(f'Received msg: {body}')
 
     for event_name, event_payload in event.items():
-        payload = event_payload['payload']
-        aggregate_id = event_payload['aggregate_id']
-        payload_type = event_payload['payload_type']
+        try:
+            payload = event_payload['payload']
+            aggregate_id = event_payload['aggregate_id']
+            payload_type = event_payload['payload_type']
 
-        handler = write_model_map.get(event_name)['handler']
-        factory_func = write_model_map.get(event_name)['factory_func']
-        event_model = factory_func(
-            aggregate_id=aggregate_id,
-            payload=payload,
-            payload_type=payload_type
-        )
-        handler(event=event_model)
+            handler = write_model_map.get(event_name)['handler']
+            factory_func = write_model_map.get(event_name)['factory_func']
+            event_model = factory_func(
+                aggregate_id=aggregate_id,
+                payload=payload,
+                payload_type=payload_type
+            )
+
+            handler(event=event_model)
+
+        except Exception:
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+            raise
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 def main():
+    queue_service_wrapper = PikaQueueServiceWrapper(
+        amqp_url=AMPQ_URL
+    )
+
+    queue_service_wrapper.create_queue(
+        queue=PERSISTENCE_WORKER_QUEUE,
+        dlq_support=True,
+        dlq_queue='BOE_ERRORS',
+        dlq_exchange='BOE_ERROR_EXCHANGE',
+        dlq_routing_key='BOE_ERROR_ROUTING_KEY'
+    )
+
     consumer = make_basic_pika_consumer(
         amqp_url=AMPQ_URL,
         queue=PERSISTENCE_WORKER_QUEUE,
         on_message_callback=on_message_callback,
     )
-    consumer.run()
+    try:
+        consumer.run()
+    except pika.exceptions.ChannelClosedByBroker:
+        consumer.run()
 
 
 if __name__ == "__main__":
