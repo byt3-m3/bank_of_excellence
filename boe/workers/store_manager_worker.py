2e16aa0c5bf5a8a32e69c9b7669dc1ba1dcce4b2
byt3-m3 <cbaxtertech@gmail.com>
2021-12-19 22:22:21 -0500

    Modified envs

--- boe/workers/store_manager_worker.py
+++ boe/workers/store_manager_worker.py
@@ -5,16 +5,17 @@ from boe.applications.store_domain_apps import (
     StoreManagerApp,
     StoreManagerAppEventFactory
 )
-from boe.env import AMPQ_URL, STORE_MANAGER_APP_QUEUE, SQLLITE_WORKER_EVENT_STORE
+from boe.env import AMPQ_URL, STORE_MANAGER_WORKER_QUEUE, STORE_MANAGER_WORKER_SQLLITE_EVENT_STORE
 from cbaxter1988_utils.log_utils import get_logger
-from cbaxter1988_utils.pika_utils import make_basic_pika_consumer
+from cbaxter1988_utils.pika_utils import make_basic_pika_consumer, PikaQueueServiceWrapper
 from pika.adapters.blocking_connection import BlockingChannel
+from pika.exceptions import ChannelClosedByBroker
 from pika.spec import Basic, BasicProperties
 
 logger = get_logger("StoreManagerApp")
 
 INFRASTRUCTURE_FACTORY = "eventsourcing.sqlite:Factory"
-SQLITE_DBNAME = SQLLITE_WORKER_EVENT_STORE
+SQLITE_DBNAME = STORE_MANAGER_WORKER_SQLLITE_EVENT_STORE
 
 os.environ['INFRASTRUCTURE_FACTORY'] = INFRASTRUCTURE_FACTORY
 os.environ['SQLITE_DBNAME'] = SQLITE_DBNAME
@@ -38,7 +39,7 @@ def on_message_callback(ch: BlockingChannel, method: Basic.Deliver, properties: 
         event_factory = event_handler_map.get(event_name)['event_factory']
         _event = event_factory(**payload)
 
-        result = handler(event=_event)
+        handler(event=_event)
         #
         logger.info(f'Processed ApplicationEvent={_event} - Handler={handler}')
 
@@ -46,13 +47,29 @@ def on_message_callback(ch: BlockingChannel, method: Basic.Deliver, properties: 
 
 
 def main():
+    queue_service_wrapper = PikaQueueServiceWrapper(
+        amqp_url=AMPQ_URL
+    )
+
+    queue_service_wrapper.create_queue(
+        queue=STORE_MANAGER_WORKER_QUEUE,
+        dlq_support=True,
+        dlq_queue='BOE_ERRORS',
+        dlq_exchange='BOE_ERROR_EXCHANGE',
+        dlq_routing_key='BOE_ERROR_ROUTING_KEY'
+    )
+
     consumer = make_basic_pika_consumer(
         amqp_url=AMPQ_URL,
-        queue=STORE_MANAGER_APP_QUEUE,
+        queue=STORE_MANAGER_WORKER_QUEUE,
         on_message_callback=on_message_callback,
     )
 
-    consumer.run()
+    try:
+
+        consumer.run()
+    except ChannelClosedByBroker:
+        consumer.run()
 
 
 if __name__ == "__main__":
