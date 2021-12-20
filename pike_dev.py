4795624b0f9be856ad6ffa05c1ec52cdb1f0b950
byt3-m3 <cbaxtertech@gmail.com>
2021-12-20 12:41:21 -0500

    Development

--- pike_dev.py
+++ pike_dev.py
@@ -1,16 +1,16 @@
 from uuid import uuid4
 
-from boe.clients.persistence_worker_client import PersistenceWorkerClient
 from boe.clients.bank_manager_worker_client import BankManagerWorkerClient
-from boe.env import AMQP_URL, BANK_MANAGER_WORKER_QUEUE, STORE_MANAGER_WORKER_QUEUE, PERSISTENCE_WORKER_QUEUE
+from boe.clients.persistence_worker_client import PersistenceWorkerClient
+from boe.env import AMQP_URL, STORE_MANAGER_WORKER_QUEUE
 from boe.lib.domains.bank_domain import BankDomainFactory, BankTransactionMethodEnum
 from boe.utils.serialization_utils import serialize_model
 from cbaxter1988_utils.pika_utils import make_basic_pika_publisher
 
-bank_manager_worker_client = BankManagerWorkerClient()
-
 
 def publish_establish_new_account_event():
+    bank_manager_worker_client = BankManagerWorkerClient()
+
     account_id = str(uuid4())
 
     bank_manager_worker_client.publish_new_bank_account_event(
@@ -21,6 +21,8 @@ def publish_establish_new_account_event():
 
 
 def publish_new_transaction_event(account_id):
+    bank_manager_worker_client = BankManagerWorkerClient()
+
     bank_manager_worker_client.publish_new_transaction_event(
         item_id="00000000-0000-0000-0000-000000000010",
         account_id=account_id,
@@ -47,13 +49,6 @@ def publish_new_store_event():
 
 
 def publish_save_aggregate_event():
-    publisher = make_basic_pika_publisher(
-        amqp_url=AMQP_URL,
-        queue=PERSISTENCE_WORKER_QUEUE,
-        exchange="P_WORKER_EXCHANGE",
-        routing_key="P_WORKER_KEY"
-    )
-
     client = PersistenceWorkerClient()
 
     bank_account_agg = BankDomainFactory.build_bank_domain_aggregate(owner_id=uuid4(), is_overdraft_protected=False)
@@ -69,7 +64,7 @@ def publish_save_aggregate_event():
 
 
 if __name__ == "__main__":
-    account_id = publish_establish_new_account_event()
-    publish_new_transaction_event(account_id=account_id)
+    # account_id = publish_establish_new_account_event()
+    # publish_new_transaction_event(account_id=account_id)
 
-    # publish_save_aggregate_event()
+    publish_save_aggregate_event()
