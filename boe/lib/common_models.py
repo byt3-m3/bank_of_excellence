d1bec8a22101ab83be1abcbda5a29a28045268db
byt3-m3 <cbaxtertech@gmail.com>
2021-12-21 15:58:11 -0500

    Intergrated pyserde to common models

--- boe/lib/common_models.py
+++ boe/lib/common_models.py
@@ -1,17 +1,21 @@
 from dataclasses import dataclass
 from uuid import UUID
+from serde import serialize, deserialize
 
-
+@serialize
+@deserialize
 @dataclass
 class Entity:
     id: UUID
 
-
+@serialize
+@deserialize
 @dataclass(frozen=True)
 class AppEvent:
     pass
 
-
+@serialize
+@deserialize
 @dataclass(frozen=True)
 class AppNotification:
     pass
