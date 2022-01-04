370d4e21e29efea55d32b61cb57b7a69606073e4
byt3-m3 <cbaxtertech@gmail.com>
2022-01-03 20:54:29 -0500

    added new task manager worker

---
+++ boe/workers/task_manager_worker.py
@@ -0,0 +1,65 @@
+import json
+
+from boe.applications.task_domain_apps import (
+    TaskManagerApp,
+    TaskDomainAppEventFactory,
+    NewTaskEvent
+)
+from boe.env import (
+    AMQP_HOST,
+    RABBITMQ_USERNAME,
+    RABBITMQ_PASSWORD,
+    TASK_MANAGER_WORKER_QUEUE
+
+)
+from boe.lib.event_register import EventMapRegister
+from boe.workers.env_setup import set_up_task_manager_worker_env
+from cbaxter1988_utils.pika_utils import make_pika_queue_consumer_v2
+from pika.adapters.blocking_connection import BlockingChannel
+from pika.spec import Basic, BasicProperties
+
+event_map_register = EventMapRegister()
+
+app = TaskManagerApp()
+
+event_map_register.register_event(
+    event_name='NewTaskEvent',
+    event_class=NewTaskEvent,
+    event_handler=app.handle_event,
+    event_factory_func=TaskDomainAppEventFactory.build_new_task_event
+)
+
+
+def on_message_callback(ch: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body):
+    event_request = json.loads(body)
+
+    for event_name, payload in event_request.items():
+        _event_factory = event_map_register.get_event_factory(event_name=event_name)
+        _event_handler = event_map_register.get_event_handler(event_name=event_name)
+
+        try:
+            _event_model = _event_factory(**payload)
+            _event_handler(event=_event_model)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception:
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+
+
+def main():
+    set_up_task_manager_worker_env()
+    consumer = make_pika_queue_consumer_v2(
+        amqp_host=AMQP_HOST,
+        amqp_password=RABBITMQ_PASSWORD,
+        amqp_username=RABBITMQ_USERNAME,
+        queue=TASK_MANAGER_WORKER_QUEUE,
+        on_message_callback=on_message_callback,
+    )
+
+    try:
+        consumer.consume(prefetch_count=1)
+    except Exception:
+        raise
+
+
+if __name__ == '__main__':
+    main()
