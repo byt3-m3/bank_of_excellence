619efd408b391042b2c6bab5d961ba26c7ad4e15
byt3-m3 <cbaxtertech@gmail.com>
2022-01-07 20:03:38 -0500

    Added mocks to test

--- tests/applications/test_task_manager_app.py
+++ tests/applications/test_task_manager_app.py
@@ -2,7 +2,11 @@ import datetime
 import uuid
 from unittest.mock import patch
 
-from boe.applications.task_domain_apps import TaskManagerApp, TaskDomainAppEventFactory, TaskStatusEnum
+from boe.applications.task_domain_apps import (
+    TaskManagerApp,
+    TaskManagerAppEventFactory,
+    TaskStatusEnum
+)
 from pytest import fixture
 
 
@@ -19,9 +23,21 @@ def task_manager_app_testable():
 
 @fixture
 def new_task_event():
-    return TaskDomainAppEventFactory.build_new_task_event(
+    return TaskManagerAppEventFactory.build_new_task_event(
         owner_id='a38c5bdc-f079-40f6-a9c5-8be227e17dfd',
         evidence_required=False,
+        due_date=datetime.datetime(year=2022, day=2, month=12),
+        description='Test Task',
+        name='TestTask',
+        value=5.00
+    )
+
+
+@fixture
+def new_task_event_evidence_required():
+    return TaskManagerAppEventFactory.build_new_task_event(
+        owner_id='a38c5bdc-f079-40f6-a9c5-8be227e17dfd',
+        evidence_required=True,
         due_date=datetime.datetime(year=2022, day=2, month=12),
         description='Test Task',
         name='TestTask',
@@ -48,11 +64,54 @@ def test_task_manager_app_when_handling_mark_task_complete_event(
 
     task_id = app.handle_event(new_task_event)
 
-    mark_task_complete_event = TaskDomainAppEventFactory.build_mark_task_complete_event(task_id=str(task_id))
+    mark_task_complete_event = TaskManagerAppEventFactory.build_mark_task_complete_event(task_id=str(task_id))
 
     app.handle_event(mark_task_complete_event)
 
     task = app.get_task_aggregate(task_id=task_id)
 
     assert task.task.status is TaskStatusEnum.complete
+    write_model_mock.assert_called()
+
+
+def test_task_manager_app_when_handling_update_task_value_event(
+        write_model_mock,
+        task_manager_app_testable,
+        new_task_event_evidence_required
+):
+    app = task_manager_app_testable
+    task_id = app.handle_event(new_task_event_evidence_required)
+
+    event = TaskManagerAppEventFactory.build_update_task_value_event(
+        task_id=str(task_id),
+        value=1000
+    )
+
+    app.handle_event(event)
+
+    aggregate = app.get_task_aggregate(task_id=task_id)
+
+    assert aggregate.task.value == 1000
+    write_model_mock.assert_called()
+
+
+def test_task_manager_app_when_handling_add_evidence_event(
+        write_model_mock,
+        task_manager_app_testable,
+        new_task_event_evidence_required
+):
+    app = task_manager_app_testable
+
+    task_id = app.handle_event(new_task_event_evidence_required)
+
+    evidence_event = TaskManagerAppEventFactory.build_add_evidence_event(
+        task_id=str(task_id),
+        data=b'This is Test Data'
+    )
+
+    app.handle_event(evidence_event)
+
+    aggregate = app.get_task_aggregate(task_id=task_id)
+
+    assert aggregate.task.evidence_data == b'This is Test Data'
     write_model_mock.assert_called()
